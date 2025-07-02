cd4ab2d3546af213b90be54d8674396f06ff4d4c
Wide-Pants <ajtwoddl1236@naver.com>
2025-07-02 16:45:33 +0900

    feat: 게시판 페이지 구현
    - 게시판 메인 페이지 및 하위 페이지들
    - 게시글 상세, 댓글, 인기 게시글 기능
    - 게시글 작성, 수정, 삭제, 신고 기능

---
+++ src/app/(main)/board/hot-post/page.tsx
@@ -0,0 +1,8 @@
+import HotPostPageContent from "@/features/board/components/HotPostPageContent";
+
+export default function HotPostPage() {
+  // 여기서 서버 사이드 데이터 fetching 가능
+  // 예: HOT 게시글 목록 초기 로딩
+
+  return <HotPostPageContent />;
+}
