d43e0d213f0ed7d28be6f321f57bd75e457d74be
Wide-Pants <ajtwoddl1236@naver.com>
2025-07-15 18:25:47 +0900

    feat: 기타 기능 페이지 구현
    - 악기 스킬 관리 API
    - 위치 기반 서비스 API
    - 알림 관리 API

---
+++ src/app/(main)/instrument-skill/add/route.ts
@@ -0,0 +1,34 @@
+import { fetchWithRefresh } from '@/core';
+export const dynamic = "force-dynamic";
+
+export async function POST(req: Request) {
+
+  try {
+
+      const proxyUrl = `${process.env.BASE_URL}/api/member/inst`;
+
+      const reqJson = await req.json();
+
+      console.log(JSON.stringify(reqJson))
+
+      const response = await fetchWithRefresh(proxyUrl, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(reqJson)
+      });
+
+      if (!response.ok) throw Error('서버 불안정' + response.status)
+
+      // 프록시 응답 받기
+      const proxyResponse = await response.json();
+
+      // 클라이언트에 프록시 응답 반환
+      return Response.json(proxyResponse, { status: 200 });
+
+  } catch (error) {
+      console.error('프록시 처리 중 에러:', error);
+      return new Response('프록시 처리 실패', { status: 500 });
+  }
+}
---
+++ src/app/(main)/notification/notReadCnt/route.ts
@@ -0,0 +1,25 @@
+import { fetchWithRefresh } from "@/core";
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  try {
+    const proxyUrl = `${process.env.BASE_URL}/api/message/fcm/count`;
+
+    const proxyResponse = await fetchWithRefresh(proxyUrl);
+
+    if (!proxyResponse.ok) {
+      const errorText = await proxyResponse.text(); // 또는 await res.json()
+      console.error("🔥 백엔드 에러 메시지:", errorText);
+      throw Error("🔥서버 불안정" + proxyResponse.status);
+    }
+
+    const data = await proxyResponse.json();
+    console.log(data);
+    const { response } = await data;
+
+    return Response.json(response, { status: 200 });
+  } catch (error) {
+    console.error("프록시 처리 중 에러:", error);
+    return new Response("프록시 처리 실패", { status: 500 });
+  }
+}
