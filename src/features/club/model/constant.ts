d6469bd428f8c8b1916cac842332164c907c308c
Wide-Pants <ajtwoddl1236@naver.com>
2025-08-15 16:17:22 +0900

    fix: 동아리 타입 및 상수 수정

--- src/features/club/model/constant.ts
+++ src/features/club/model/constant.ts
@@ -1,12 +1,12 @@
-export const CLUB_NAMES = [
+export const CLUB_NAMES: ClubName[] = [
   "어흥",
   "떼",
   "하날다래",
   "악반",
   "푸른소래",
+  "산틀",
 ] as const;
 
-export type ClubName = (typeof CLUB_NAMES)[number];
 
 export const SCHOOL_NAME = ["상명대", "연대", "이화여대", "홍대", "고대"] as const;
 
@@ -18,6 +18,7 @@ const clubToSchoolNameMap: Record<ClubName, SchoolName> = {
   하날다래: "이화여대",
   악반: "홍대",
   푸른소래: "고대",
+  산틀: "홍대",
 } as const;
 
 const clubToClubIdMap: Record<ClubName, number> = {
@@ -26,6 +27,7 @@ const clubToClubIdMap: Record<ClubName, number> = {
   하날다래: 3,
   악반: 4,
   푸른소래: 5,
+  산틀: 6,
 } as const;
 
 export const mapClubToClubId = (club: ClubName): number => {
